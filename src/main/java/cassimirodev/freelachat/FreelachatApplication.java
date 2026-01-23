57b38f0ddb600c69ac51eb3d8af73484ab60a8cc
Luís Eduardo <luiseduardocass06@gmail.com>
2026-01-23 00:33:46 -0300

    feat: initial project files

---
+++ src/main/java/cassimirodev/freelachat/FreelachatApplication.java
@@ -0,0 +1,13 @@
+package cassimirodev.freelachat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelachatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelachatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cassimirodev/freelachat/FreelachatApplicationTests.java
@@ -0,0 +1,13 @@
+package cassimirodev.freelachat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelachatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
