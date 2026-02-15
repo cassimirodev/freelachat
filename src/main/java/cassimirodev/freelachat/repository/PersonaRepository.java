4de89c73e0b2e9cb6b31fdcd06145f3dda7e8417
Luís Eduardo <luiseduardocass06@gmail.com>
2026-02-15 02:07:17 -0300

    feat: add datafaker lib

--- src/main/java/cassimirodev/freelachat/repository/PersonaRepository.java
+++
@@ -1,9 +0,0 @@
-package cassimirodev.freelachat.repository;
-
-import cassimirodev.freelachat.model.Persona;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonaRepository extends JpaRepository<Persona, Long> {
-}
