1a7c00522df9e4c9e01e9c0c6f89bbef6dcf6a58
Luís Eduardo <luiseduardocass06@gmail.com>
2026-02-15 02:17:50 -0300

    feat: add a factory to generate new random personas

---
+++ src/main/java/cassimirodev/freelachat/factory/PersonaFactory.java
@@ -0,0 +1,31 @@
+package cassimirodev.freelachat.factory;
+
+
+import cassimirodev.freelachat.component.PersonalityPersonaPromptTemplate;
+import cassimirodev.freelachat.config.FakerConfig;
+import cassimirodev.freelachat.enums.PersonaType;
+import cassimirodev.freelachat.model.Persona;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PersonaFactory {
+
+    private FakerConfig faker;
+
+
+    public Persona createRandomPersona() {
+        PersonaType type = PersonaType.getRandomPersonality();
+        String systemPrompt = PersonalityPersonaPromptTemplate.getPersonalityPromptToPersona(type);
+
+        Persona persona = new Persona();
+
+        persona.setName(faker.data().name().fullName());
+        persona.setType(type);
+        persona.setSystemPrompt(systemPrompt);
+
+        return persona;
+    }
+
+
+}
