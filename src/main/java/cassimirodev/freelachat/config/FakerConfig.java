e87fbe511d1f2983d7721fec8029f89e32bb7eaa
Luís Eduardo <luiseduardocass06@gmail.com>
2026-02-15 02:15:59 -0300

    feat: add a datafaker bean and a config to this bean

---
+++ src/main/java/cassimirodev/freelachat/config/FakerConfig.java
@@ -0,0 +1,16 @@
+package cassimirodev.freelachat.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import net.datafaker.Faker;
+
+import java.util.Locale;
+
+@Configuration
+public class FakerConfig {
+
+    @Bean
+    public Faker data() {
+        return new Faker(Locale.of("pt-BR"));
+    }
+}
