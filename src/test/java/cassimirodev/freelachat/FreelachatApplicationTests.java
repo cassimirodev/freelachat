e532d65c638012a727ae2ead955deeedfbca897c
Luís Eduardo <luiseduardocass06@gmail.com>
2026-02-15 02:27:27 -0300

    feat: associates test application properties

--- src/test/java/cassimirodev/freelachat/FreelachatApplicationTests.java
+++ src/test/java/cassimirodev/freelachat/FreelachatApplicationTests.java
@@ -2,8 +2,10 @@ package cassimirodev.freelachat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FreelachatApplicationTests {
 
 	@Test
