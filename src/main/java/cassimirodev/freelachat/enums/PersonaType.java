0f5218ad833248809bbf93cc6335ba5350ccb4b0
Luís Eduardo <luiseduardocass06@gmail.com>
2026-02-15 02:19:21 -0300

    feat: add a method to return random PersonaType Enum values

--- src/main/java/cassimirodev/freelachat/enums/PersonaType.java
+++ src/main/java/cassimirodev/freelachat/enums/PersonaType.java
@@ -1,9 +1,22 @@
 package cassimirodev.freelachat.enums;
 
+import java.util.List;
+import java.util.Random;
+
 public enum PersonaType {
     BRAVA,
     CALMA,
     IMPACIENTE,
     EXPERIENTE,
-    ANSIOSA
+    ANSIOSA;
+
+    static final PersonaType[] values = PersonaType.values();
+    private static final Random random = new Random();
+
+    /*
+      O random é uma classe que gera números aleatórios. Combinado com o nextInt(), ele vai gerar um numero aleatório entre 0 e a quantidade passada por values.length.
+     */
+    public static PersonaType getRandomPersonality() {
+        return values[random.nextInt(values.length)];
+    }
 }
