3af37a212799033d3e46f5b9c897104662e5cabf
Luís Eduardo <luiseduardocass06@gmail.com>
2026-02-15 02:23:46 -0300

    fix: fixes id argument type

--- src/main/java/cassimirodev/freelachat/repository/ChatRepository.java
+++ src/main/java/cassimirodev/freelachat/repository/ChatRepository.java
@@ -4,6 +4,8 @@ import cassimirodev.freelachat.model.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface ChatRepository extends JpaRepository<Chat, Long> {
+public interface ChatRepository extends JpaRepository<Chat, UUID> {
 }
